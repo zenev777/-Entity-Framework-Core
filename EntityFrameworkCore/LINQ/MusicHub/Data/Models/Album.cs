38f38271ea1cc803d1a6c82c5868ee9fb4199376
Zhan <zhanenev6@gmail.com>
2023-11-19 13:41:29 +0200

    Almost ready first exercise

---
+++ EntityFrameworkCore/LINQ/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicHub.Data.Models
+{
+    public class Album
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal Price => Songs.Sum(s=>s.Price);
+
+        public virtual ICollection<Song> Songs { get; set; }
+
+        public int ProducerId { get; set; }
+        [ForeignKey(nameof(ProducerId))]
+        public Producer Producer { get; set; }
+
+
+    }
+}
---
+++ EntityFrameworkCore/LINQ/MusicHub/Data/Models/SongPerformer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicHub.Data.Models
+{
+    public class SongPerformer
+    {
+        [ForeignKey(nameof(SongId))]
+        public int SongId { get; set; }
+        public Song Song { get; set; }
+
+        [ForeignKey(nameof(PerformerId))]
+        public int PerformerId { get; set; }
+        public Performer Performer { get; set; }
+    }
+}
